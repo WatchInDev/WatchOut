5d59b471c89d9bcc1ec248b64faf229bbe506e8c
Michał Puchyr <michalpuchyr3@gmail.com>
2025-08-09 17:07:36 +0200

    ZPI-18 chore: add swagger to backend

--- Server/src/main/kotlin/org/zpi/watchout/app/controller/EventTypeController.kt
+++ Server/src/main/kotlin/org/zpi/watchout/app/controller/EventTypeController.kt
@@ -1,5 +1,6 @@
 package org.zpi.watchout.app.controller
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -12,10 +13,10 @@ class EventTypeController(
     private val eventTypeService: EventTypeService
 ) {
 
-    @RequestMapping
+    @GetMapping
     fun getAllEventTypes() = eventTypeService.getAllEventTypes()
 
-    @RequestMapping("/{name}")
+    @GetMapping("/{name}")
     fun getEventTypeByName(@PathVariable name: String) = eventTypeService.getEventTypeByName(name)
 
 }
