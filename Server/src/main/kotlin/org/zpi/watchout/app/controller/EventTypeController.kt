51d63203a5828a4f770ed2ee38f1abb4008bf611
Michał Puchyr <michalpuchyr3@gmail.com>
2025-08-15 15:59:51 +0200

    ZPI-11 chore: install scalar UI for api docs

--- Server/src/main/kotlin/org/zpi/watchout/app/controller/EventTypeController.kt
+++ Server/src/main/kotlin/org/zpi/watchout/app/controller/EventTypeController.kt
@@ -1,5 +1,7 @@
 package org.zpi.watchout.app.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,14 +11,17 @@ import org.zpi.watchout.service.EventTypeService
 
 @RestController
 @RequestMapping("/api/v1/event-types")
+@Tag(name = "Event types", description = "Event types management")
 class EventTypeController(
     private val eventTypeService: EventTypeService
 ) {
 
     @GetMapping
+    @Operation(summary = "Get all event types")
     fun getAllEventTypes() = eventTypeService.getAllEventTypes()
 
     @GetMapping("/{name}")
+    @Operation(summary = "Get event type by name")
     fun getEventTypeByName(@PathVariable name: String) = eventTypeService.getEventTypeByName(name)
 
 }
