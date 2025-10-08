cc28729b428eae3b422526ad73ea97c6479d9010
Michał Puchyr <michalpuchyr3@gmail.com>
2025-10-08 21:46:10 +0200

    ZPI-56 style: fix font weight not being properly interpreted when set as number

--- Client/watchout/components/Base/Text.tsx
+++ Client/watchout/components/Base/Text.tsx
@@ -7,24 +7,31 @@ const defaultStyles = {
 const convertFontWeightToFontFamily = (fontWeight: string | number | undefined) => {
   switch (fontWeight) {
     case 'black':
+    case '900':
     case 900:
       return { fontFamily: 'Poppins_900Black' };
     case 'extrabold':
+    case '800':
     case 800:
       return { fontFamily: 'Poppins_800ExtraBold' };
     case 'bold':
+    case '700':
     case 700:
       return { fontFamily: 'Poppins_700Bold' };
     case 'semibold':
+    case '600':
     case 600:
       return { fontFamily: 'Poppins_600SemiBold' };
     case 'medium':
+    case '500':
     case 500:
       return { fontFamily: 'Poppins_500Medium' };
     case 'regular':
+    case '400':
     case 400:
       return { fontFamily: 'Poppins_400Regular' };
     case 'light':
+    case '300':
     case 300:
       return { fontFamily: 'Poppins_300Light' };
     default:
