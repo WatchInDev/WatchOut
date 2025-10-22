9be4228f227b6c8b1300fd3ef55bb24d2d6a4362
Michał Puchyr <michalpuchyr3@gmail.com>
2025-10-22 18:22:58 +0200

    ZPI-98 feat: implement adding new comments

---
+++ Client/watchout/features/events/comments/useCreateComment.ts
@@ -0,0 +1,9 @@
+import { useMutation } from "@tanstack/react-query";
+import { apiClient } from "utils/apiClient";
+import { API_ENDPOINTS } from "utils/apiDefinition";
+
+export const useCreateComment = () => useMutation({
+  mutationFn: async ({ eventId, content }: { eventId: number; content: string }) => {
+    return apiClient.post(API_ENDPOINTS.comments.create(eventId), { content });
+  }
+});
