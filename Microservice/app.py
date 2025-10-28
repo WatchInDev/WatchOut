abae629ade64b0966605a023df69b7a3a39a57e2
Aleksandr <103767386+avtorgenii@users.noreply.github.com>
2025-10-28 15:43:24 +0100

    ZPI-97 feat: prepared scraping outages for backend

--- Microservice/app.py
+++ Microservice/app.py
@@ -1,17 +1,13 @@
-import os
-import asyncio
-import json
 from datetime import datetime, timedelta
 from scrapers import get_tauron_planned_shutdowns, get_energa_planned_shutdowns
 
 
-async def electricityOutagesFetching() -> None:
+def electricity_outages_fetching():
     try:
         tauron = get_tauron_planned_shutdowns(datetime.now(), datetime.now() + timedelta(days=7))
         energa = get_energa_planned_shutdowns()
 
-
+        return {"tauron": tauron, "energa": energa}
 
     except Exception as e:
         print(f"Exception during data scraping or retrieving: {e}")
-
