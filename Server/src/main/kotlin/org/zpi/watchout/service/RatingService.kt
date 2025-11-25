23c64482cd40d4c73af7dee782275fad7b447678
Aleksandr <103767386+avtorgenii@users.noreply.github.com>
2025-11-25 13:59:59 +0100

    ZPI 129/130 feat: changed func arguments for bunch of functions

--- Server/src/main/kotlin/org/zpi/watchout/service/RatingService.kt
+++ Server/src/main/kotlin/org/zpi/watchout/service/RatingService.kt
@@ -4,19 +4,20 @@ import org.springframework.stereotype.Service
 import org.zpi.watchout.data.entity.CommentRating
 import org.zpi.watchout.data.repos.CommentRatingRepository
 import org.zpi.watchout.data.repos.EventRatingRepository
+import org.zpi.watchout.service.dto.RatingRequestDTO
 
 @Service
 class RatingService (private val commentRatingRepository: CommentRatingRepository, private val eventRatingRepository: EventRatingRepository, private val geoService: GeoService) {
 
-    fun upsertCommentRating(userId: Long, commentId: Long, ratingValue: Int, eventId: Long, userLat: Double, userLng: Double) {
-        if (geoService.isUserWithinDistance(eventId, userLat, userLng)) {
-            commentRatingRepository.upsertRating(userId, commentId, ratingValue)
+    fun upsertCommentRating(userId: Long, commentId: Long, eventId: Long, ratingRequestDTO: RatingRequestDTO) {
+        if (geoService.isUserWithinDistance(eventId, ratingRequestDTO.latitude, ratingRequestDTO.longitude)) {
+            commentRatingRepository.upsertRating(userId, commentId, ratingRequestDTO.rating)
         }
     }
 
-    fun upsertEventRating(userId: Long, eventId: Long, ratingValue: Int, userLat: Double, userLng: Double) {
-        if (geoService.isUserWithinDistance(eventId, userLat, userLng)) {
-            eventRatingRepository.upsertRating(userId, eventId, ratingValue)
+    fun upsertEventRating(userId: Long, eventId: Long, ratingRequestDTO: RatingRequestDTO) {
+        if (geoService.isUserWithinDistance(eventId, ratingRequestDTO.latitude, ratingRequestDTO.longitude)) {
+            eventRatingRepository.upsertRating(userId, eventId, ratingRequestDTO.rating)
         }
     }
 
